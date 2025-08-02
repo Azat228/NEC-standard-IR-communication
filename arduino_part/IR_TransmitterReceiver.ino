d3f4a3cb8d267736b1f99bdaaf68a47d202456f9
Azat <47598184+Azat228@users.noreply.github.com>
2025-08-02 20:37:25 +0800

    final version

--- arduino_part/IR_TransmitterReceiver.ino
+++ arduino_part/IR_TransmitterReceiver.ino
@@ -1,80 +1,62 @@
 #include <IRremote.hpp>
-#define IR_RECEIVE_PIN 7
-#define IR_LED_PIN 2
-#define Send_Pin 8
-void setup()
-{
-  //...
+#define IR_RECEIVE_PIN 11
+#define IR_LED_PIN 9
+#define SEND_PIN 8
+
+void setup() {
   Serial.begin(9600);
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   IrSender.begin(IR_LED_PIN);
-   Serial.println("Enter a string and press Enter:");
+  pinMode(SEND_PIN, OUTPUT);   // If you plan to use a strobe pin
+  digitalWrite(SEND_PIN, LOW); // Set to LOW initially
+  Serial.println("Enter a digit (0-9), '+' or '-' for command:");
 }
 
 void loop() {
-    digitalWrite(Send_Pin, HIGH);
-  
-     if (Serial.available() > 0) {
-    String userInput = Serial.readStringUntil('\n'); // Read until newline
-    userInput.trim(); // Remove any whitespace or newline characters
-    
-    if (userInput.length() > 0 ) { // Only proceed if we got actual input
+  // For IR transmission
+  if (Serial.available() > 0) {
+    String userInput = Serial.readStringUntil('\n');
+    userInput.trim();
+
+    if (userInput.length() > 0 ) {
       Serial.print("You entered: ");
       Serial.println(userInput);
-      
-      switch(userInput[0]) { // Check only the first character
-        case '0':
-          IrSender.sendNEC(0x00FF6897, 32);
-          break;
-        case '1':
-          IrSender.sendNEC(0x00FF30CF, 32);
-          break;
-        case '2':
-          IrSender.sendNEC(0x00FF18E7, 32);
-          break;
-        case '3':
-          IrSender.sendNEC(0x00FF7A85, 32);
-          break;
-        case '4':
-          IrSender.sendNEC(0x00FF10EF, 32);
-          break;
-        case '5':
-          IrSender.sendNEC(0x00FF38C7, 32);
-          break;
-        case '6':
-          IrSender.sendNEC(0x00FF5AA5, 32);
-          break;
-        case '7':
-          IrSender.sendNEC(0x00FF42BD , 32);
-          break;
-        case '8':
-          IrSender.sendNEC(0x00FF4AB5, 32);
-          break;
-        case '9':    
-          IrSender.sendNEC(0x00FF52AD , 32);
-          break;
-        case '-':    
-          IrSender.sendNEC(0x00FFE01F   , 32);
-          break;
-        case '+':    
-          IrSender.sendNEC(0x00FFA857   , 32);
-          break;
-        
+
+      unsigned long irCode = 0;
+      switch(userInput[0]) {
+        case '0': irCode = 0x00FF6897; break;
+        case '1': irCode = 0x00FF30CF; break;
+        case '2': irCode = 0x00FF18E7; break;
+        case '3': irCode = 0x00FF7A85; break;
+        case '4': irCode = 0x00FF10EF; break;
+        case '5': irCode = 0x00FF38C7; break;
+        case '6': irCode = 0x00FF5AA5; break;
+        case '7': irCode = 0x00FF42BD; break;
+        case '8': irCode = 0x00FF4AB5; break;
+        case '9': irCode = 0x00FF52AD; break;
+        case '-': irCode = 0x00FFE01F; break;
+        case '+': irCode = 0x00FFA857; break;
         default:
-          Serial.println("Please enter a digit (0-9)");
-          break;
+          Serial.println("Please enter a digit (0-9), '+' or '-'.");
+          return;
       }
+      // Optional: Pulse SEND_PIN as a strobe
+      digitalWrite(SEND_PIN, HIGH);
+      IrSender.sendNEC(irCode, 32);
+      digitalWrite(SEND_PIN, LOW);
+
+      Serial.print("IR code sent: 0x"); Serial.println(irCode, HEX);
     }
   }
-  if (IrReceiver.decode()) {
-      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
-      IrReceiver.printIRResultShort(&Serial); // Print complete received data
-      IrReceiver.printIRSendUsage(&Serial);   // Print what's required to send this data
-     
-      IrReceiver.resume(); // Enable receiving of the next value
-  }
-  
 
-delay(1000);
-  //...
+  // For IR reception
+  if (IrReceiver.decode()) {
+    Serial.print("IR received: 0x");
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    IrReceiver.printIRResultShort(&Serial);
+    IrReceiver.printIRSendUsage(&Serial);
+    IrReceiver.resume();
+  }
+
+  delay(100); // Shorter delay for responsiveness
 }
